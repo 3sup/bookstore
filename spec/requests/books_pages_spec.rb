546d96bf6efd2df4ad309a12a6c7b955a3d7ff5f
3sup <gsgiuseppe99@gmail.com>
2013-11-18 18:10:01 +0100

    Fixed CSS syntax, replaced tests

--- spec/requests/books_pages_spec.rb
+++ spec/requests/books_pages_spec.rb
@@ -4,7 +4,7 @@ describe "Homepage" do
   describe "GET /books_pages" do
     it "Has the footer menu" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-      get 'books/index'
+      get 'books'
       expect(page).to have_content("Authors")
     end
   end
